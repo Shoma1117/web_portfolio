689f9aeeb0381729fc2cbdf6634e479b28a6f007
Shoma1117 <yabukishoma1117@gmail.com>
2026-02-08 10:49:23 +0900

    [add]フォント追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,18 @@
 import "./globals.css";
+import { JetBrains_Mono } from "next/font/google";
+import { Noto_Sans_JP } from "next/font/google";
+
+const mono = JetBrains_Mono({
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-jetBrainsMono"
+});
+
+const noto = Noto_Sans_JP({
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-notoSans"
+});
 
 export default function RootLayout({
   children,
@@ -6,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
-      <body className="bg-bg text-text">
+    <html lang="ja" className={`${mono.variable} ${noto.variable}`}>
+      <body className="bg-bg text-text font-base">
         {children}
       </body>
     </html>
