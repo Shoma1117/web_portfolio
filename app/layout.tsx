646b4aba855b6b17c5c6195aca9b5f5b85dfcc45
Shoma1117 <yabukishoma1117@gmail.com>
2026-02-08 23:04:27 +0900

    [add]サイドバーレスポンシブ対応

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { JetBrains_Mono } from "next/font/google";
 import { Noto_Sans_JP } from "next/font/google";
-import SidebarWrapper from "./_components/sidebar/wrapper";
+import NavigationLayout from "./_components/sidebar/navigation-layout";
 import SidebarContents from "./_components/sidebar/contents"
 
 const mono = JetBrains_Mono({
@@ -24,10 +24,10 @@ export default function RootLayout({
   return (
     <html lang="ja" className={`${mono.variable} ${noto.variable}`}>
       <body className="bg-bg text-text font-base">
-        <div className="flex h-screen">
-          <SidebarWrapper>
+        <div className="flex h-dvh">
+          <NavigationLayout>
             <SidebarContents />
-          </SidebarWrapper>
+          </NavigationLayout>
           <main className="flex-1 overflow-y-auto">
             {children}
           </main>
