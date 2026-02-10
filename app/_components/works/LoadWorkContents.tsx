498cde3adf195dc4f08ad6dc4c09291c1b4cfab9
Shoma1117 <yabukishoma1117@gmail.com>
2026-02-10 23:22:51 +0900

    [add]Workセクション作成

---
+++ app/_components/works/LoadWorkContents.tsx
@@ -0,0 +1,22 @@
+import { WorkContents, workData } from "./workData";
+import Work from "./work"; 
+import fs from "fs";
+import path from "path";
+
+export default function LoadWorkContents() {
+    const wc: WorkContents = {};
+    //同期的にで.mdファイルを読み込み、名前と内容を紐づけたRecordを作成する
+    workData.forEach(work => {
+        const filePath = path.join(process.cwd(), `app/_components/works/contents/${work.slug}.md`);
+        try {
+            wc[work.slug] = fs.readFileSync(filePath, "utf-8");
+        }
+        catch {
+            wc[work.slug] = "";
+        }
+    });
+
+    return (
+        <Work workContents={wc}/>
+    )
+}
---
+++ app/_components/works/workData.tsx
@@ -0,0 +1,91 @@
+export const CATEGORYS = {
+    game: "GAME",
+    web: "WEB"
+} as const;
+
+export type WorkCategory = "Game" | "Web";
+
+export type WorkItem = {
+    name: string;       //作品名
+    thumbnail: string;      //サムネイル
+    slug: string;       //表示するmdファイル名
+    category: WorkCategory;     //GameかWebのカテゴリ
+};
+
+const rogeDangion: WorkItem = {
+    name: "Roge_Dangion",
+    thumbnail: "/works/thumbnail-rouge.webp",
+    slug: "roge_Dangion",
+    category: "Game"
+};
+
+const amidaChangeRoute: WorkItem = {
+    name: "あみだ Change ルート",
+    thumbnail: "/works/thumbnail-amida.webp",
+    slug: "amidaChangeRoute",
+    category: "Game"
+};
+
+const quickGunman: WorkItem = {
+    name: "早打ちガンマン",
+    thumbnail: "/works/thumbnail-gunman.webp",
+    slug: "quickGunman",
+    category: "Game"
+};
+
+const defenceTreasure: WorkItem = {
+    name: "defence treasure!",
+    thumbnail: "/works/thumbnail-defence.webp",
+    slug: "defenceTreasure",
+    category: "Game"
+};
+
+const neoJumpCity: WorkItem = {
+    name: "ネオジャンプシティ",
+    thumbnail: "/works/thumbnail-neoJump.webp",
+    slug: "neoJumpCity",
+    category: "Game"
+};
+
+const anchorGravity: WorkItem = {
+    name: "AnchorGravity",
+    thumbnail: "/works/thumbnail-anchor.webp",
+    slug: "anchorGravity",
+    category: "Game"
+};
+
+const behaviorTree: WorkItem = {
+    name: "BehaviorTree",
+    thumbnail: "/works/thumbnail-behavior.webp",
+    slug: "behaviorTree",
+    category: "Game"
+};
+
+const onlineQuiz: WorkItem = {
+    name: "OnlineQuiz",
+    thumbnail: "/works/thumbnail-min-sider.webp",
+    slug: "onlineQuiz",
+    category: "Web"
+};
+
+const portfolio: WorkItem = {
+    name: "Webポートフォリオ",
+    thumbnail: "/works/thumbnail-portfolio.webp",
+    slug: "portfolio",
+    category: "Web"
+};
+
+export const workData: WorkItem[] = [
+    rogeDangion,
+    amidaChangeRoute,
+    quickGunman,
+    defenceTreasure,
+    neoJumpCity,
+    anchorGravity,
+    behaviorTree,
+    onlineQuiz,
+    portfolio
+];
+
+//mdファイル名と内容の紐づけ
+export type WorkContents = Record<string, string>;
