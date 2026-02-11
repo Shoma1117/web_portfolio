dfb4ea17b658369859b7288c5ad913ac25dfad69
Shoma1117 <yabukishoma1117@gmail.com>
2026-02-12 04:26:44 +0900

    [add]モーダル内のマークダウン対応とhtml対応

--- app/_components/mdModal.tsx
+++ app/_components/mdModal.tsx
@@ -1,6 +1,9 @@
 import Modal from "react-modal";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
+import rehypeHighlight from "rehype-highlight";
+import 'highlight.js/styles/atom-one-dark.css'
 
 type ModalProps = {
   isOpen: boolean;
@@ -19,13 +22,13 @@ export default function MdModal({
       isOpen={isOpen}
       onRequestClose={closeModal}
       ariaHideApp={false}
-      className="bg-bg-sub border border-border rounded-lg"
-      overlayClassName="bg-black/50 fixed inset-0"
+      className="bg-bg-sub border-4 border-border rounded-lg max-h-[600px] overflow-y-scroll p-4" 
+      overlayClassName="bg-black/50 fixed inset-0 fixed inset-0 flex items-center justify-center"
     >
       <button onClick={closeModal}>閉じる</button>
 
-      <div className="prose">
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>{mdContents}</ReactMarkdown>
+      <div className="prose prose-invert prose-pre:bg-transparent">
+        <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw, rehypeHighlight]}>{mdContents}</ReactMarkdown>
       </div>
     </Modal>
   );
