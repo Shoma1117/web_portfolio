675c72b7bb188988e5a93a27068d869388286fc0
Shoma1117 <yabukishoma1117@gmail.com>
2026-02-11 10:09:19 +0900

    [add]remark-gfmとtailwindcss/typography追加

--- app/_components/mdModal.tsx
+++ app/_components/mdModal.tsx
@@ -1,29 +1,32 @@
 import Modal from "react-modal";
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 type ModalProps = {
-    isOpen: boolean;
-    mdContents: string;
-    closeModal: () => void;
+  isOpen: boolean;
+  mdContents: string;
+  closeModal: () => void;
 };
 
 //マークダウンの中身を受け取り、モーダル内で表示する
-export default function MdModal({ isOpen, mdContents, closeModal }: ModalProps) {
-    return (
-        <Modal
-            isOpen={isOpen}
-            onRequestClose={closeModal}
-            ariaHideApp={false}
-            className="bg-bg-sub border border-border rounded-lg"
-            overlayClassName="bg-black/50 fixed inset-0"
-        >
-            <button onClick={closeModal}>
-                閉じる
-            </button>
+export default function MdModal({
+  isOpen,
+  mdContents,
+  closeModal,
+}: ModalProps) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={closeModal}
+      ariaHideApp={false}
+      className="bg-bg-sub border border-border rounded-lg"
+      overlayClassName="bg-black/50 fixed inset-0"
+    >
+      <button onClick={closeModal}>閉じる</button>
 
-            <ReactMarkdown>
-                {mdContents}
-            </ReactMarkdown>
-        </Modal>
-    )
+      <div className="prose">
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>{mdContents}</ReactMarkdown>
+      </div>
+    </Modal>
+  );
 }
